757bf91d9b8ca3f864c4dd8bee3dc3ccfd373306
ferza <velixsion@gmail.com>
2020-07-29 20:12:15 +0700

    update

---
+++ coba.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>TEST</title>
+    </head>
+    <body>
+        <?php
+
+            echo"Hello World";
+        ?>
+        <br>
+        <?php
+            print"Halo Dunia";
+
+        ?>
+        <hr>
+        <?php
+            $nama='Ferza Nanda';
+            $alamat='Lambaro Skep';
+            $no_wa=6282276722299;
+
+            echo "Nama Kamu : $nama <br> Alamat Kamu : $alamat <br> No Wa Kamu : +$no_wa";
+        ?>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $usernama="ani";
+        $password="admin";
+
+        if($usernama=="ani"&&$password="admin"){
+            echo"selamat datang $usernama";
+        }else{
+            echo"username Atau password anda salah";
+        }
+    ?>
+</body>
+</html>
